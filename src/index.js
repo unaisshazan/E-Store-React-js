7d375126ef1e5d49921fc07cf99399e4d55a6b4a
unais ali <51183141+unaisshazan@users.noreply.github.com>
2020-07-14 15:08:51 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import thunk from 'redux-thunk';
-import {createStore, compose, applyMiddleware} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import {BrowserRouter} from 'react-router-dom';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
