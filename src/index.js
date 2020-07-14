5689b96093f8649f699eb4da3a69c7a152795519
unais ali <51183141+unaisshazan@users.noreply.github.com>
2020-07-14 13:44:36 +0500

    add .

--- src/index.js
+++ src/index.js
@@ -1,41 +1,29 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import {Provider} from 'react-redux';
-import thunk from 'redux-thunk';
-import {createStore, compose, applyMiddleware} from 'redux';
-import {BrowserRouter} from 'react-router-dom';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import './index.css';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { combineReducers, createStore } from 'redux';
+import cartReducer from './ducks/cart';
+import productsReducer from './ducks/products';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
-import shopReducer from './store/reducers/shop';
-import {Elements, StripeProvider} from 'react-stripe-elements';
+import productsData from './data/products';
+import 'bootstrap/dist/css/bootstrap.css';
 
-// setup redux dev tools to use our app state
-// enable these to use thunk chrome dev tool
-// const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
-// composeEnhancers prevents using promises in actions, if you find a solution please share
-// const store = createStore(shopReducer, composeEnhancers(applyMiddleware(thunk)));
-const store = createStore(shopReducer, applyMiddleware(thunk));
+const rootReducer = combineReducers({
+    cart: cartReducer,
+    products: productsReducer
+});
 
-const app = (
+let store = createStore(
+    rootReducer,
+    {
+        products: productsData // initial store values
+    },
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() // for debugging
+);
 
+render(
     <Provider store={store}>
-        <BrowserRouter>
-            {/*StripeProvider initializes the stripe and passes in the publishable key*/}
-            <StripeProvider apiKey="pk_test_TYooMQauvdEDq54NiTphI7jx">
-                <Elements>
-                    <App/>
-                </Elements>
-            </StripeProvider>
-        </BrowserRouter>
-    </Provider>
-
-)
-
-ReactDOM.render(app, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
